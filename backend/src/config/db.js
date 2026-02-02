7409d4277c83081a91aa431788130d779b2f182c
Alphi-aravind <lakshmiaravid.s@alphind.com>
2026-02-02 12:27:46 +0530

    fix: update environment variables and login view

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,10 +1,13 @@
-import mysql from 'mysql2/promise';
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'ngo_data_hub',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
-  connectionLimit: 10
+  connectionLimit: 10,
 });
